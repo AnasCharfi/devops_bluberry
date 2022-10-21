7bfbd1ceef84557f91de91ef39dfcd45a3c35f54
welhazi cyrine <cyrine.welhazi@esprit.tn>
2022-10-21 17:13:03 +0100

    Elimination de vulnerability  *addStock with DTO

--- src/main/java/tn/esprit/rh/achat/entities/StockModel.java
+++ src/main/java/tn/esprit/rh/achat/entities/StockModel.java
@@ -1,20 +1,13 @@
 package tn.esprit.rh.achat.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import tn.esprit.rh.achat.entities.Produit;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.io.Serializable;
 import java.util.Set;
 
 public class StockModel {
-        public String libelleStock;
-        public Integer qte;
-        public Integer qteMin;
-        public  Set<Produit> produits;
+         String libelleStock;
+         Integer qte;
+         Integer qteMin;
+       Set<Produit> produits;
 
 
 }
