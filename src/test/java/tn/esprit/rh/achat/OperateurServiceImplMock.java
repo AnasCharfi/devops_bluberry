f4bafdaf33f9c2f576748a4fbcc01c4551005dcf
thourayamazlout <thouraya.mazlout@esprit.tn>
2022-10-31 13:07:25 +0100

    Junit and mockito

---
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplMock.java
@@ -0,0 +1,79 @@
+package tn.esprit.rh.achat;
+
+import org.hibernate.mapping.Any;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.util.Assert;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+import tn.esprit.rh.achat.services.OperateurServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class OperateurServiceImplMock {
+    @Mock
+    OperateurRepository repo;
+    @InjectMocks
+    OperateurServiceImpl OperateurService;
+ Operateur o = Operateur.builder().nom("thouraya").prenom("mazlout").password("pwd").build();
+ List<Operateur> listop = new ArrayList<Operateur>(){
+     {
+         add( Operateur.builder().nom("Thouraya1").prenom("Mazlout1").password("PWD51").build());
+         add(Operateur.builder().nom("Thouraya2").prenom("Mazlout2").password("PWD52").build());
+
+     }
+
+ };
+
+
+    @Test
+    void TestRetrieveOperateur(){
+        Mockito.when(repo.findById(Mockito.anyLong())).thenReturn(Optional.of(o));
+        Operateur o1 = OperateurService.retrieveOperateur(Long.valueOf("1"));
+        Assertions.assertNotNull(o1);
+
+    }
+
+    @Test
+    void TestRetrieveAllOperateur() {
+        Mockito.when(repo.findAll()).thenReturn(listop);
+        int listOperateurs = OperateurService.retrieveAllOperateurs().size();
+        Assertions.assertEquals(listop.size() , OperateurService.retrieveAllOperateurs().size());
+
+    }
+
+
+    @Test
+    public void TestAddOperateur(){
+        Mockito.when(repo.save(o)).thenReturn(o);
+        Operateur O= OperateurService.addOperateur(o);
+        Assertions.assertNotNull(O);
+    }
+
+    @Test
+    void TestdeleteOperateur(){
+        OperateurService.deleteOperateur(1l);
+        Mockito.verify(repo).deleteById(1l);
+        int listOperateurs = OperateurService.retrieveAllOperateurs().size();
+        Assertions.assertEquals(listOperateurs,OperateurService.retrieveAllOperateurs().size());
+    }
+
+    @Test
+    void TestUpdateOperateur(){
+        Mockito.when(repo.save(o)).thenReturn(o);
+        Operateur ou= OperateurService.updateOperateur(o);
+        Assertions.assertNotNull(ou);
+    }
+
+}
---
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
@@ -0,0 +1,59 @@
+package tn.esprit.rh.achat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+import tn.esprit.rh.achat.services.IOperateurService;
+
+@SpringBootTest
+class OperateurServiceImplTest {
+
+    @Autowired
+    IOperateurService OprateurServiceImpl;
+    OperateurRepository repo;
+    @Test
+    @Rollback(true)
+    public void TestAddOperateur(){
+        Operateur O= OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
+                .password("PWD5").build());
+        Assertions.assertNotNull(O);
+    }
+
+    @Test
+    @Rollback(false)
+    void TestRetrieveAllOperateur(){
+        int listOperateurs = OprateurServiceImpl.retrieveAllOperateurs().size();
+        OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
+                .password("PWD5").build());
+        Assertions.assertEquals(listOperateurs+1,OprateurServiceImpl.retrieveAllOperateurs().size());
+    }
+
+    @Test
+    void TestdeleteOperateur(){
+        long id=2;
+        int listOperateurs = OprateurServiceImpl.retrieveAllOperateurs().size();
+        OprateurServiceImpl.deleteOperateur(id);
+        Assertions.assertEquals(listOperateurs-1,OprateurServiceImpl.retrieveAllOperateurs().size());
+    }
+
+    @Test
+    @Rollback(false)
+    void TestRetrieveOperateur(){
+        long id=3;
+        Operateur o = OprateurServiceImpl.retrieveOperateur(id);
+        Assertions.assertNotNull(o);
+    }
+
+    @Test
+    @Rollback(false)
+    void TestUpdateOperateur(){
+        Operateur o = OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
+                .password("PWD5").build());
+         Operateur ou= OprateurServiceImpl.updateOperateur(o);
+        Assertions.assertNotNull(ou);
+    }
+
+}
