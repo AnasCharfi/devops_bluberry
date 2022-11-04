3908c3f9c03a562281ea253ff4bf03616e16f672
thourayamazlout <thouraya.mazlout@esprit.tn>
2022-11-04 19:39:43 +0100

    tests update

--- src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
@@ -14,49 +14,47 @@ class OperateurServiceImplTest {
     @Autowired
     IOperateurService OprateurServiceImpl;
     OperateurRepository repo;
+
     @Test
-    void contextLoads() {
-        // empty test that would fail if our Spring configuration does not load correctly
-    }
-    /*
     @Rollback(true)
-    public void TestAddOperateur(){
-        Operateur O= OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
+    public void TestAddOperateur() {
+        Operateur O = OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
                 .password("PWD5").build());
         Assertions.assertNotNull(O);
     }
 
     @Test
     @Rollback(false)
-    void TestRetrieveAllOperateur(){
+    void TestRetrieveAllOperateur() {
         int listOperateurs = OprateurServiceImpl.retrieveAllOperateurs().size();
         OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
                 .password("PWD5").build());
-        Assertions.assertEquals(listOperateurs+1,OprateurServiceImpl.retrieveAllOperateurs().size());
+        Assertions.assertEquals(listOperateurs + 1, OprateurServiceImpl.retrieveAllOperateurs().size());
     }
 
     @Test
-    void TestdeleteOperateur(){
-        long id=4;
+    void TestdeleteOperateur() {
+        long id = 4;
         int listOperateurs = OprateurServiceImpl.retrieveAllOperateurs().size();
         OprateurServiceImpl.deleteOperateur(id);
-        Assertions.assertEquals(listOperateurs-1,OprateurServiceImpl.retrieveAllOperateurs().size());
+        Assertions.assertEquals(listOperateurs - 1, OprateurServiceImpl.retrieveAllOperateurs().size());
     }
 
     @Test
     @Rollback(false)
-    void TestRetrieveOperateur(){
-        long id=4;
+    void TestRetrieveOperateur() {
+        long id = 4;
         Operateur o = OprateurServiceImpl.retrieveOperateur(id);
         Assertions.assertNotNull(o);
     }
 
     @Test
     @Rollback(false)
-    void TestUpdateOperateur(){
+    void TestUpdateOperateur() {
         Operateur o = OprateurServiceImpl.addOperateur(Operateur.builder().nom("Thouraya").prenom("Mazlout")
                 .password("PWD5").build());
-         Operateur ou= OprateurServiceImpl.updateOperateur(o);
+        Operateur ou = OprateurServiceImpl.updateOperateur(o);
         Assertions.assertNotNull(ou);
-    }*/
+    }
+
 }
