ae5fe15183f9ae3060f3be436a6d16fb8712be78
welhazi cyrine <cyrine.welhazi@esprit.tn>
2022-11-10 17:45:11 +0100

    Modification ajout de test config

--- src/test/java/tn/esprit/rh/achat/StockTest.java
+++ src/test/java/tn/esprit/rh/achat/StockTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -22,6 +23,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ContextConfiguration
 public class StockTest {
 
     @Autowired
