87fa0516cc9605f04f320217f85fec99245f7910
welhazi cyrine <cyrine.welhazi@esprit.tn>
2022-11-11 19:29:30 +0100

    mock runner ya rabi tekhdem aad

--- src/test/java/tn/esprit/rh/achat/StockMockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/StockMockitoTest.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @Slf4j
 public class StockMockitoTest {
