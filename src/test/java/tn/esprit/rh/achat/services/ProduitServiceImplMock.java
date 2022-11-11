056fbf160553d46dbe3d01b0cb7b44c9db843d7e
unknown <maryem.khechine@esprit.tn>
2022-11-11 20:45:01 +0100

    'd'

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
